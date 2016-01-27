0a58cee002acd9475aa1b3c430683447b3a17f76
alpz5566 <451846271@qq.com>
2016-01-27 16:07:44 +0800

    no permission page

--- src/com/book/library/controller/IndexController.java
+++ src/com/book/library/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.book.library.controller;
 
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +13,7 @@ public class IndexController {
 	 * 测试用例
 	 * @return
 	 */
+	@RequiresPermissions("item:query111")
 	@RequestMapping(value="/home")
 	public String toIndexPage(){
 		return "index";
